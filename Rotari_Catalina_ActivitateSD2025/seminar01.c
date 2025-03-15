b1d5d516bea406aba5f4f61e1a2432d03710527b
Rotari02 <catalina.rotari02@gmail.com>
2025-03-15 21:17:13 +0200

    Seminar 1 SDD Rotari Catalina 6 martie 2025

--- Rotari_Catalina_ActivitateSD2025/seminar01.c
+++ Rotari_Catalina_ActivitateSD2025/seminar01.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-int main() {
-	printf("Salutare!\n Introduceti un numar intreg: ");
-	int variabila = 0;
-	scanf_s("%d", &variabila);
-	printf("Ai introdus: %d", variabila);
-	//citirea unei variabile reale.
-	return 0;
-}
+//#include <stdio.h>
+//int main() {
+//	printf("Salutare!\n Introduceti un numar intreg: ");
+//	int variabila = 0;
+//	scanf_s("%d", &variabila);
+//	printf("Ai introdus: %d", variabila);
+//	//citirea unei variabile reale.
+//	return 0;
+//}

5715122a8706c5bbafaba9a9e12e532054c412ac
Rotari02 <catalina.rotari02@gmail.com>
2025-03-15 21:39:15 +0200

    Modificare denumire seminarii

--- Rotari_Catalina_ActivitateSD2025/seminar2.c
+++ Rotari_Catalina_ActivitateSD2025/seminar2.c
@@ -1,127 +1,109 @@
-
-
-
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//trebuie sa folositi fisierul masini.txt
-//sau va creati un alt fisier cu alte date
-
-struct StructuraMasina {
-	int id;
-	int nrUsi;
-	float pret;
-	char* model;
-	char* numeSofer;
-	unsigned char serie;
-};
-typedef struct StructuraMasina Masina;
-
-void afisareMasina(Masina masina) {
-	//afiseaza toate atributele unei masini
-	printf("Id: %d\nnrUsi: %d\npret: %0.2f\nmodel: %s\nnumeSofer: %s\nserie: %c", masina.id, masina.nrUsi, masina.pret, masina.model, masina.numeSofer, masina.serie);
-}
-
-void afisareVectorMasini(Masina* masini, int nrMasini) {
-	//afiseaza toate elemente de tip masina din vector
-	//prin apelarea functiei afisareMasina()
-	for (int i = 0; i < nrMasini; i++) {
-		afisareMasina(masini[i]);
-	}
-}
-
-void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
-	//adauga in vectorul primit o noua masina pe care o primim ca parametru
-	//ATENTIE - se modifica numarul de masini din vector;
-	Masina* vector = malloc(sizeof(Masina) * ((*nrMasini) + 1));
-
-	for (int i = 0; i < (*nrMasini); i++) {
-		vector[i] = (*masini)[i];
-	}
-	vector[(*nrMasini)] = masinaNoua;
-	vector[(*nrMasini)].model = malloc(sizeof(char) * (strlen(masinaNoua.model) + 1));
-	strcpy_s(vector[(*nrMasini)].model, (strlen(masinaNoua.model) + 1), masinaNoua.model);
-	vector[(*nrMasini)].numeSofer = malloc(sizeof(char) * (strlen(masinaNoua.numeSofer) + 1));
-	strcpy_s(vector[(*nrMasini)].numeSofer, (strlen(masinaNoua.numeSofer) + 1), masinaNoua.numeSofer);
-
-	free(*masini);
-	(*masini) = vector;
-	(*nrMasini)++;
-
-}
-
-Masina citireMasinaFisier(FILE* file) {
-	//functia citeste o masina dintr-un strceam deja deschis
-	//masina citita este returnata;
-	Masina masina;
-	char buffer[100];
-	fgets(buffer, 100, file);
-	char separator[] = ",\n";
-
-	char* token = strtok(buffer, separator);
-	masina.id = atoi(token);
-
-	char* token2 = strtok(NULL, separator);
-	masina.nrUsi = atoi(token2);
-
-	char* token3 = strtok(NULL, separator);
-	masina.pret = atof(token3);
-
-	char* token4 = strtok(NULL, separator);
-	masina.model = malloc(sizeof(char) * (strlen(token4) + 1));
-	strcpy_s(masina.model, strlen(token4) + 1, token4);
-
-	char* token5 = strtok(NULL, separator);
-	masina.numeSofer = malloc(sizeof(char) * (strlen(token5) + 1));
-	strcpy_s(masina.numeSofer, strlen(token5) + 1, token5);
-
-	char* token6 = strtok(NULL, separator);
-	masina.serie = token6[0];
-
-	return masina;
-}
-
-Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
-	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
-	FILE* f = fopen(numeFisier, "r");
-	Masina* masini = NULL;
-	while (!feof(f)) {
-		//prin apelul repetat al functiei citireMasinaFisier()
-		Masina m = citireMasinaFisier(f);
-		//numarul de masini este determinat prin numarul de citiri din fisier
-		adaugaMasinaInVector(&masini, nrMasiniCitite, m);
-		free(m.model);
-		free(m.numeSofer);
-	}
-	fclose(f);
-	return masini;
-
-	//ATENTIE - la final inchidem fisierul/stream-ul
-
-}
-
-//temaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
-	//este dezalocat intreg vectorul de masini
-	for (int i = 0; i < *nrMasini; i++) {
-		free((*vector)[i].model);
-		free((*vector)[i].numeSofer);
-	}
-	free(*vector);
-	*vector = NULL;
-	*nrMasini = 0;
-
-}
-
-int main() {
-	Masina* masini = NULL;
-	int nrMasini = 0;
-	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
-	afisareVectorMasini(masini, nrMasini);
-	return 0;
-}
-
+//#include<stdio.h>
+//#include<malloc.h>
+//
+//struct Masina {
+//	int id;
+//	char* model;
+//	float tonaj;
+//	char serie;
+//};
+//struct Masina initializare(int id, const char* model, float tonaj, char serie) {
+//	//initializare structura 
+//	struct Masina s;
+//	s.id = 1;
+//	s.tonaj = tonaj;
+//	s.serie = serie;
+//	s.model = malloc(sizeof(char) * (strlen(model) + 1));
+//	strcpy_s(s.model, (strlen(model) + 1), model);
+//	return s;
+//}
+//
+//void afisare(struct Masina s) {
+//	//afisarea tuturor atributelor.
+//	printf("\nId: %d\nmodel: %s\ntonaj: %.2f\nserie:%c", s.id, s.model, s.tonaj, s.serie);
+//}
+//
+//void afisareVector(struct Masina* vector, int nrElemente) {
+//	//afisarea elementelor din vector apeland functia afisare
+//	for (int i = 0; i < nrElemente; i++) {
+//		afisare(vector[i]);
+//	}
+//}
+//
+//struct Masina* copiazaPrimeleNElemente(struct Masina* vector, int nrElemente, int nrElementeCopiate) {
+//	//copiem intr-un vector nou pe care il vom returna, primele nrElementeCopiate
+//	struct Masina* vectorNou = malloc(sizeof(struct Masina) * nrElementeCopiate);
+//	for (int i = 0; i < nrElementeCopiate; i++) {
+//		vectorNou[i] = vector[i];
+//		vectorNou[i].model = malloc(sizeof(char) * (strlen(vector[i].model) + 1));
+//		strcpy_s(vectorNou[i].model, strlen(vector[i].model) + 1, vector[i].model);
+//	}
+//
+//	return vectorNou;
+//}
+//
+//void dezalocare(struct Masina** vector, int* nrElemente) {
+//	//dezalocam elementele din vector si vectorul
+//	for (int i = 0; i < *nrElemente; i++) {
+//		free((*vector)[i].model); // primeiro dereferenciamos e depois acessamos o objeto e o atributo especifico
+//	}
+//	free(*vector);
+//	*vector = NULL;
+//	*nrElemente = 0;
+//}
+//
+//void copiazaAnumiteElemente(struct Masina* vector, char nrElemente, float prag, struct Masina** vectorNou, int* dimensiune) {
+//	//parametrul prag poate fi modificat in functie de 
+//	// tipul atributului ales pentru a indeplini o conditie
+//	//este creat un nou vector cu elementele care indeplinesc acea conditie
+//	*dimensiune = 0;
+//	for (int i = 0; i < nrElemente; i++) {
+//		if (vector[i].tonaj < prag) {
+//			(*dimensiune)++; //colocamos entre parenteses, para nao passar para o proximo elemento e sim adicionar 1 ao elemento
+//		}
+//	}
+//	(*vectorNou) = malloc(sizeof(struct Masina) * (*dimensiune));
+//	int k = 0;
+//	for (int i = 0; i < nrElemente; i++) {
+//		if (vector[i].tonaj < prag) {
+//			(*vectorNou)[k] = vector[i];
+//			(*vectorNou)[k].model = malloc(sizeof(char) * (strlen(vector[i].model) + 1));
+//			strcpy_s((*vectorNou)[k].model, strlen(vector[i].model) + 1, vector[i].model);
+//			k++;
+//		}
+//	}
+//}
+//
+////temaaaaa, usar strcmp
+//struct Masina getPrimulElementConditionat(struct Masina* vector, int nrElemente, const char* conditie) {
+//	//trebuie cautat elementul care indeplineste o conditie
+//	//dupa atributul de tip char*. Acesta este returnat.
+//	struct Masina s;
+//	s.id = 1;
+//
+//	return s;
+//}
+//
+//
+//
+//int main() {
+//	struct Masina m;
+//	m = initializare(1, "audi", 100.45, 's');
+//	afisare(m);
+//	struct Masina* vectorMasini;
+//	int nrMasini = 3;
+//	vectorMasini = malloc(sizeof(struct Masina) * nrMasini);
+//	vectorMasini[0] = initializare(2, "BMW", 100.45, 's');
+//	vectorMasini[1] = initializare(3, "Dacia", 1.45, 'a');
+//	vectorMasini[2] = initializare(4, "Renault", 10.45, 'b');
+//	afisareVector(vectorMasini, nrMasini);
+//	int nrelementeCopiate = 2;
+//	struct Masina* vectorMasiniCopiate = copiazaPrimeleNElemente(vectorMasini, nrMasini, nrelementeCopiate);
+//	afisareVector(vectorMasiniCopiate, nrelementeCopiate);
+//	dezalocare(&vectorMasiniCopiate, &nrelementeCopiate);
+//	int dimensiune;
+//	copiazaAnumiteElemente(vectorMasini, nrMasini, 11.00, &vectorMasiniCopiate, &dimensiune);
+//	printf("\n");
+//	afisareVector(vectorMasiniCopiate, dimensiune);
+//	return 0;
+//}
